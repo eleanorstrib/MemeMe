32694cece5b8cfea5a8da1ed5d11c57a2287ed6d
eleanorstrib <eleanor@sloan.mit.edu>
2015-09-08 17:03:22 -0700

    images from camera and album now appear in the ViewController, cancel button works; need to fix contstraints to get this working in portrait mode

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -23,30 +23,29 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
     }
 
-
     // camera button code
     @IBAction func cameraActivate(sender: UIButton) {
         imagePicker.sourceType = .Camera
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
-
-    
-    
     // select an image code
     @IBAction func albumActivate(sender: UIButton) {
+        // opens album on phone
         imagePicker.sourceType = .PhotoLibrary
         presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]){
         if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage! {
+            // remove auto fit for selected image in the view to keep aspect ratio and remove image from the dictionary
             self.photoPreview.contentMode = .ScaleAspectFit
             self.photoPreview.image = selectedImage
         }
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    // dismiss the image picker if the user presses cancel
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
