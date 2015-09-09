30851e3dbe8fafbe94ff79bcecdf0e37c3e3fdf3
eleanorstrib <eleanor@sloan.mit.edu>
2015-09-08 17:00:34 -0700

    images from camera and album now appear in the ViewController, need to fix contstraints to get this working in portrait mode

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
-    @IBOutlet var photoPreview: UIView!
+
+    @IBOutlet weak var photoPreview: UIImageView!
     @IBOutlet weak var pickFromAlbum: UIToolbar!
     @IBOutlet weak var cameraButton: UITableViewCell!
     @IBOutlet weak var albumButton: UIButton!
@@ -19,24 +20,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    // camera button code
     @IBAction func cameraActivate(sender: UIButton) {
         imagePicker.sourceType = .Camera
         presentViewController(imagePicker, animated: true, completion: nil)
     }
+    
 
+    
+    
+    // select an image code
     @IBAction func albumActivate(sender: UIButton) {
         imagePicker.sourceType = .PhotoLibrary
         presentViewController(imagePicker, animated: true, completion: nil)
-        
     }
-
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]){
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage! {
+            self.photoPreview.contentMode = .ScaleAspectFit
+            self.photoPreview.image = selectedImage
+        }
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
 
