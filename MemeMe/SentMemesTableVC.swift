ae023577d4ebdd3e28445f17f7821a491fd1ad30
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-18 08:48:27 -0700

    made updates to SentMemesTableVC file

--- MemeMe/SentMemesTableVC.swift
+++ MemeMe/SentMemesTableVC.swift
@@ -17,16 +17,14 @@ class SentMemesTableVC: UITableViewController, UITableViewDataSource, UITableVie
         return savedMemes
     }
     
-    var deleteMeme: NSIndexPath? = nil
-    
     override func tableView(tableview: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.sentMemes.count
+        return sentMemes.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //create the reusable cell and define data source
         let cell = tableView.dequeueReusableCellWithIdentifier("SentMemeCell") as! UITableViewCell
-        let meme = self.sentMemes[indexPath.row]
+        let meme = sentMemes[indexPath.row]
         
         //set the text and image for the table
         cell.textLabel?.text = meme.topText + " " + meme.bottomText
@@ -37,45 +35,45 @@ class SentMemesTableVC: UITableViewController, UITableViewDataSource, UITableVie
     
     // swipe to delete functionality in the table view
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-            let meme = self.sentMemes[indexPath.row]
-            if editingStyle == .Delete {
-                savedMemes.removeAtIndex(indexPath.row)
-                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
-            }
+        let meme = sentMemes[indexPath.row]
+        if editingStyle == .Delete {
+            savedMemes.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
     }
     
-    
-
     //reload data in table so that memes created via collection VC show up
     override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated)
         tableView.reloadData()
     }
     
-    // show navbar and toolbar with this view, hide when user creates meme
+    // show navbar toolbar with this view, hide when user creates meme; hide back button
     override func viewDidAppear(animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
-        self.tabBarController?.tabBar.hidden = false
-        self.navigationItem.setHidesBackButton(true, animated: true)
+        super.viewDidAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+        tabBarController?.tabBar.hidden = false
+        navigationItem.setHidesBackButton(true, animated: true)
     }
     
     override func viewDidDisappear(animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: animated)
-        self.tabBarController?.tabBar.hidden = true
+        super.viewDidDisappear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+        tabBarController?.tabBar.hidden = true
     }
     
-    
+    // go to meme editing VC (plus sign)
     @IBAction func makeNewMeme(sender: UIBarButtonItem) {
-        self.performSegueWithIdentifier("makeMemeTable", sender: self)
+        performSegueWithIdentifier("makeMemeTable", sender: self)
     }
     
+    // go to meme detail view
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let detailVC = self.storyboard!.instantiateViewControllerWithIdentifier("MemeDetailView") as! MemeDetailView
         
-        detailVC.sentMemes = self.sentMemes[indexPath.row]
-        self.navigationController!.pushViewController(detailVC, animated: true)
+        detailVC.sentMemes = sentMemes[indexPath.row]
+        navigationController!.pushViewController(detailVC, animated: true)
     }
-    
-    
     
 }
 
